28d72e1639d5eba82c94a4afb39427d22947c0b6
Yasser Morteza <yasser.morteza@gmail>
2019-07-30 19:45:58 +0200

    table days manipulation

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -8,8 +8,55 @@ const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 class Calendar extends Component {
 
-    render() {
+    state = {
+        calendarElements: undefined
+    }
 
+    componentDidMount() {
+        const { calendarElements } = this.state;
+
+        if (!calendarElements) {
+            this.setCalendar();
+        }
+    }
+
+    setCalendar = () => {
+        const { today, numberofDays, firstDay } = this.props;
+        const tdElements = [];
+
+        for(let i = 1; i <= 31; i++) {
+            tdElements.push(<td key={i}><div><p>{i}</p></div></td>); 
+        }
+
+        const orderedTdElements = [];
+        let counter = 0;
+
+        for(let i = 0; i <= 6; i++) {
+            orderedTdElements.push([])
+            for(let j = 0; j <= 6; j++) {
+                if (i === 0 && j >= 2) {
+                    orderedTdElements[i].push(tdElements[counter++]); 
+                } else if (i > 0 ) {
+                    orderedTdElements[i].push(tdElements[counter++]); 
+                } else {
+                    orderedTdElements[i].push(<td key={`${i}_${j}`}></td>); 
+                }
+            }
+        }
+
+        this.setState({
+            calendarElements: orderedTdElements
+        })
+    }
+
+    render() {
+        const { calendarElements } = this.state;
+
+        if (!calendarElements) {
+            return (
+                <Spinner name="ball-spin-fade-loader" color="#00ffff" className="spinner" />
+            );
+        }
         return(
             <table>
                 <thead>
@@ -17,6 +64,13 @@ class Calendar extends Component {
                       {weekDays.map(day => <th key={day}>{day}</th>)}  
                     </tr>
                 </thead>
+                <tbody>
+                   {calendarElements.map((element, index) => 
+                        (<tr key={`${element}_${index}`}>
+                            {element}
+                        </tr>)
+                   )} 
+                </tbody>
             </table>
         )
     }
